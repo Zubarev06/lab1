9987fb37a0a2e298fed7081c54536a40d4634e62
Zubarev06 <pashazub2006@gmail.com>
2026-09-23 12:21:49 +0700

    delete template

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -42,28 +42,5 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier)
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    ComposeDemoTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting("Compose", modifier = Modifier.padding(innerPadding))
-        }
-    }
-}
-
-@Composable
-fun DemoScreen(modifier: Modifier) {
-    TODO("Not yet implemented")
-}
-
-
 
 
